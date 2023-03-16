14ed606a61f712745a31dd8b4cb7d3a807173b97
WQMPDev <126835603+WQMPDev@users.noreply.github.com>
2023-03-16 15:26:18 -0600

    Add files via upload

---
+++ WQMP_Create_Table_Record.sql
@@ -0,0 +1,1 @@
+CREATE TABLE `Records`(  `RecordId` bigint NOT NULL AUTO_INCREMENT,  `DeviceId` varchar(16) NOT NULL,  `TDS` integer unsigned NOT NULL,  `TBD` integer unsigned NOT NULL,  `pH` float unsigned NOT NULL,  `Fluoro` float unsigned NOT NULL,  `Flow` integer unsigned NOT NULL,  CONSTRAINT PK_SensorLogs PRIMARY KEY (`RecordId`));
