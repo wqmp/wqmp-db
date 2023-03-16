bc9013e4070b8a30982475e90829b7bc364a74be
WQMPDev <126835603+WQMPDev@users.noreply.github.com>
2023-03-16 15:26:48 -0600

    Update and rename WQMP_Create_Table_Record.sql to Create_Table_Record.sql

--- WQMP_Create_Table_Record.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE `Records`(  `RecordId` bigint NOT NULL AUTO_INCREMENT,  `DeviceId` varchar(16) NOT NULL,  `TDS` integer unsigned NOT NULL,  `TBD` integer unsigned NOT NULL,  `pH` float unsigned NOT NULL,  `Fluoro` float unsigned NOT NULL,  `Flow` integer unsigned NOT NULL,  CONSTRAINT PK_SensorLogs PRIMARY KEY (`RecordId`));
